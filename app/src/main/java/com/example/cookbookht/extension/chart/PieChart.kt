00d17e644d10b5c61a56afb647bbc0984e42c397
hunght2808 <hunght28081@gmail.com>
2022-10-30 19:48:19 +0700

    update chart

---
+++ app/src/main/java/com/example/cookbookht/extension/chart/PieChart.kt
@@ -0,0 +1,20 @@
+package com.example.cookbookht.extension.chart
+
+fun nutrientToGam(unit: String, amount: Double): Float {
+    var gam = 0.0
+    when (unit) {
+        "mg" -> {
+            gam = amount / 1000
+        }
+        "µg" -> {
+            gam = amount / 1000000
+        }
+        "IU" -> {
+            gam = amount / 30000000
+        }
+//        "kcal" -> {
+//            gam = amount * 0.033
+//        }
+    }
+    return gam.toFloat()
+}
